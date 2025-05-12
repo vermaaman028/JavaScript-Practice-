82c108fe64c63187d9448ff10e13c3568154a87f
AMAN VERMA <144582282+vermaaman028@users.noreply.github.com>
2025-05-12 04:35:41 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as a newest version
+
+//alert(2+2); // we are using node js, not browser
+
+console.log("aman verma");
+console.log(3+3)
+
+// Number ranen 2 ki power of 53
+// String 
+// Boolean
+//null => standalone value
+// Undefined
+//Symbol => uniqness
+//Object
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
