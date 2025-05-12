2f0e3441188f243444eeeb1f2694b6c312c2fd46
AMAN VERMA <144582282+vermaaman028@users.noreply.github.com>
2025-05-12 04:59:01 +0000

    conversion of datatypr

---
+++ basics/conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number (score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // Nan means not in number
+
+
+let someString = 33
+let stringNumber = String(someString)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
